61cab36ff331a004338402c81e60255e085b57de
XIELEVENXVISIXTEEN <126922347+XIELEVENXVISIXTEEN@users.noreply.github.com>
2023-06-07 23:35:48 +0000

    first

---
+++ src/js/component/CardOne.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardOne = () => {
+
+
+    return (  
+ <div className="appCard">
+    <h5 className="card-title"></h5>
+  </div>
+    )
+}
+ 
+export default CardOne;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import propTypes from "prop-types";
+import ReactDom from "react-dom";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+// import CardOne from "./CardOne";
+import CardOne from "../component/CardOne.jsx"
 //create your first component
-const Home = () => {
+const Home = ({counter}) => {
+	
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="col-2 mx-2">{counter.num1}</div>
+			<div className="col-2 mx-2">{counter.num2}</div>
+			<div className="col-2 mx-2">{counter.num3}</div>
+			<div className="col-2 mx-2">{counter.num4}</div>
+			<div className="col-2 mx-2">{counter.num5}</div>
+			<div className="col-2 mx-2">{counter.num6}</div>
 		</div>
 	);
-};
+}
 
 export default Home;
+Home.propTypes={
+	num1:propTypes.string,
+	num2:propTypes.string,
+	num3:propTypes.string,
+	num4:propTypes.string,
+	num5:propTypes.string,
+	num6:propTypes.string,
+}
